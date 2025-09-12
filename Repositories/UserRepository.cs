431ba4a02edf2cfa8ea1ed9f12408c1d67c45d8a
Jefferson Maia <jefferson.reis88@gmail.com>
2025-09-12 16:49:15 -0300

    UserRepository Created

---
+++ Repositories/UserRepository.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.EntityFrameworkCore;
+using UserApi.Data;
+using UserApi.Entities;
+
+namespace UserApi.Repositories
+{
+    public class UserRepository
+    {
+        private readonly UserDataContext _userContext;
+
+        public UserRepository(UserDataContext userContext)
+        {
+            _userContext = userContext;
+        }
+
+        public void Created(UserEntity entity)
+        {
+            _userContext.Add(entity);
+            _userContext.SaveChanges();
+                        
+        }
+
+        public List<UserEntity> GetUsers()
+        {
+            return _userContext.Users.ToList();
+            
+        }
+
+        public UserEntity ReadById(int id) 
+        {
+            return _userContext.Users.AsNoTracking().FirstOrDefault(x=> x.Id == id);         
+
+        }
+
+        public void Updated(UserEntity entity) 
+        {
+           
+            _userContext.Users.Update(entity);
+            _userContext.SaveChanges();
+
+        }
+
+        public void Delete(UserEntity entity)
+        {
+         
+            _userContext.Users.Remove(entity);
+
+        }
+    }
+}

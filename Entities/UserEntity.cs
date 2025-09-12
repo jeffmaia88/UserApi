52ce943d9a5adf5079a3e6f47d5eb31a4fa3586c
Jefferson Maia <jefferson.reis88@gmail.com>
2025-09-12 14:52:34 -0300

    Entities Created

---
+++ Entities/UserEntity.cs
@@ -0,0 +1,13 @@
+﻿namespace UserApi.Entities
+{
+    public class UserEntity
+    {
+        public int Id { get; set; }
+        public string Nome { get; set; }
+        public string Sobrenome { get; set; }
+        public string Cpf { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+
+    }
+}

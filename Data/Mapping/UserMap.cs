98b577fa6730192cce7aa5832c76d88f46ecf93c
Jefferson Maia <jefferson.reis88@gmail.com>
2025-09-12 15:32:31 -0300

    Mapping Created

---
+++ Data/Mapping/UserMap.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using UserApi.Entities;
+
+namespace UserApi.Data.Mapping
+{
+    public class UserMap : IEntityTypeConfiguration<UserEntity>
+    {
+        public void Configure(EntityTypeBuilder<UserEntity> builder)
+        {
+            builder.ToTable("User");
+
+            builder.HasKey(x => x.Id);
+
+            builder.Property(x => x.Id).ValueGeneratedOnAdd().UseIdentityColumn();
+            builder.Property(x => x.Nome).HasColumnName("Nome").HasColumnType("NVARCHAR").HasMaxLength(80).IsRequired();
+            builder.Property(x => x.Sobrenome).HasColumnName("Sobrenome").HasColumnType("NVARCHAR").HasMaxLength(120).IsRequired();
+            builder.Property(x => x.Cpf).HasColumnName("CPF").HasColumnType("VARCHAR").HasMaxLength(11).IsRequired();
+            builder.Property(x => x.Email).HasColumnName("Email").HasColumnType("NVARCHAR").HasMaxLength(80).IsRequired();
+            builder.Property(x => x.Password).HasColumnName("PasswordHash").HasColumnType("NVARCHAR").HasMaxLength(255).IsRequired();
+
+            builder.HasIndex(x => x.Cpf, "IX_User_Cpf").IsUnique();
+
+        }
+    }
+}
---
+++ Data/UserDataContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using UserApi.Entities;
+
+namespace UserApi.Data
+{
+    public class UserDataContext : DbContext
+    {
+        public UserDataContext(DbContextOptions<UserDataContext> options) : base(options)
+        {
+            
+        }
+
+        public DbSet<UserEntity> Users { get; set; }
+
+
+    }
+}

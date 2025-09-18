e0eb5b5e5d0219bb82cb015e52077548ce52eebe
Jefferson Maia <jefferson.reis88@gmail.com>
2025-09-18 13:20:47 -0300

    Update Controller e MemoryCache

--- Controllers/UserController.cs
+++ Controllers/UserController.cs
@@ -1,8 +1,8 @@
-﻿using UserApi.Services;
+﻿
+using UserApi.Services;
 using UserApi.Models;
 using Microsoft.AspNetCore.Mvc;
 using UserApi.Extensions;
-using Microsoft.Extensions.Caching.Memory;
 
 namespace UserApi.Controllers
 {
@@ -11,37 +11,25 @@ namespace UserApi.Controllers
     public class UserController : ControllerBase
     {
         private readonly UserService _userService;
-        private readonly IMemoryCache _memoryCache;
 
-        
-        private static readonly List<string> _userCacheKeys = new();
-
-        public UserController(UserService userService, IMemoryCache memoryCache)
+        public UserController(UserService userService)
         {
             _userService = userService;
-            _memoryCache = memoryCache;
         }
 
         [HttpGet]
         public async Task<IActionResult> GetAll([FromQuery] int page = 0, [FromQuery] int pageSize = 10)
         {
-            var cacheKey = $"users_page_{page}_size_{pageSize}";
-
-            
-            if (!_userCacheKeys.Contains(cacheKey))
-                _userCacheKeys.Add(cacheKey);
-
             try
             {
-                var users = await _memoryCache.GetOrCreateAsync(cacheKey, async entry =>
-                {
-                    entry.AbsoluteExpirationRelativeToNow = TimeSpan.FromHours(1);
+                var allUsers = await _userService.GetAllUsers();
+                var pagedUsers = allUsers
+                    .OrderBy(x => x.Nome)
+                    .Skip(page * pageSize)
+                    .Take(pageSize)
+                    .ToList();
 
-                    var allUsers = await _userService.GetAllUsers();
-                    return allUsers.OrderBy(x => x.Nome).Skip(page * pageSize).Take(pageSize).ToList();
-                });
-
-                return Ok(new UserResult<List<UserResponse>>(users));
+                return Ok(new UserResult<List<UserResponse>>(pagedUsers));
             }
             catch
             {
@@ -79,12 +67,6 @@ namespace UserApi.Controllers
             try
             {
                 var response = await _userService.CreateUser(user);
-
-                
-                foreach (var key in _userCacheKeys)
-                    _memoryCache.Remove(key);
-                _userCacheKeys.Clear();
-
                 return Ok(new UserResult<UserResponse>(response.Data));
             }
             catch
@@ -105,12 +87,6 @@ namespace UserApi.Controllers
             try
             {
                 var response = await _userService.UpdateUser(id, user);
-
-                
-                foreach (var key in _userCacheKeys)
-                    _memoryCache.Remove(key);
-                _userCacheKeys.Clear();
-
                 return Ok(response);
             }
             catch
@@ -128,12 +104,6 @@ namespace UserApi.Controllers
             try
             {
                 var response = await _userService.DeleteUser(id);
-
-                
-                foreach (var key in _userCacheKeys)
-                    _memoryCache.Remove(key);
-                _userCacheKeys.Clear();
-
                 return Ok(response);
             }
             catch
